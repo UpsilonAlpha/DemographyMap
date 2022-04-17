35acb19bce4bd91482fc718ee8af9137abe0b2f9
Ulban Adhikary <46319463+UpsilonAlpha@users.noreply.github.com>
2022-04-17 15:51:17 +1000

    Initial commit

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\oolba\\Documents\\Projects\\Coding\\DemographyMap\\demograpy-map\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\oolba\\Documents\\Projects\\Coding\\DemographyMap\\demograpy-map\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\oolba\\Documents\\Projects\\Coding\\DemographyMap\\demograpy-map\\src\\pages\\index.js"))
+}
+
